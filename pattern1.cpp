16be69387d2333b76fe63a0b4db1426b3e765a0f
chiragV1 <chiragverma525@gmail.com>
2022-04-09 21:40:12 +0530

    pattern2

--- pattern1.cpp
+++ pattern1.cpp
@@ -9,10 +9,11 @@ int n;
  cin>>n;
  int count =1;
     for(int i =0;i<n;i++){
-        for(int j =0;j<n;j++){
+        for(int j =0;j<=i;j++){
 
             cout<<count;
             count+=1;
+            
 
         }
         cout<<endl;
