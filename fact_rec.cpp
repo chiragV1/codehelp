5aecbd3ec53f99a0c726186d28445362374cea23
chiragV1 <chiragverma525@gmail.com>
2022-07-19 22:29:08 +0530

    all about recurssion

---
+++ fact_rec.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int fact(int n)
+{
+
+    if (n == 0)
+        return 1;
+
+    else
+    {
+
+        int ans = n * fact(n - 1);
+        return ans;
+    }
+    return 0;
+}
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    int ans = fact(n);
+
+    cout << ans << endl;
+
+    return 0;
+}
--- practice.cpp
+++ practice.cpp
@@ -4,12 +4,39 @@ using namespace std;
 
 int main()
 {
-    int a = 10;
-    int *p = &a;
-    int **q = &p;
-    int b = 20;
-    *q = &b;
-    (*p)++;
-    cout << a << " " << b << endl;
+    /*
+    int i = 5;
+    int &j = i;
+
+    cout << i << endl;
+    cout << ++i << endl;
+    cout << ++j << endl;
+    */
+    int row;
+    cin>>row;
+
+    int col;
+    cin>>col;
+
+    int ** arr = new int*[row];
+    
+    for(int i = 0; i<row ; i++){
+        arr[i] = new int[col];
+    }
+
+    for(int i = 0; i<row ;i++){
+        for(int j =0;j<col;j++){
+            cin>>arr[i][j];
+        }
+    }
+    
+    cout<<endl;
+
+    for(int i =0;i<row;i++){
+        for(int j =0;j<col;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
     return 0;
 }
