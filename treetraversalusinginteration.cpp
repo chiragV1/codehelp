00844c5311702a67e1730008cbac881a8be97f57
chiragV1 <chiragverma525@gmail.com>
2022-09-10 01:27:26 +0530

    ..

--- treetraversalusinginteration.cpp
+++ treetraversalusinginteration.cpp
@@ -33,29 +33,40 @@ node* buildtree(node* root){
 }
 
 vector<int> inorder(node* root) {
-    stack<node*> s;
-    vector<int> in;
-
-    while(true) {
-        if(root != NULL) {
-            s.push(root);
-            root = root->left;
-
-        }
-
-        else{
-            if(s.empty()) break;
-
-            node* temp = s.top();
-            s.pop();
-
-            in.push_back(temp->data);
-
-            root = root->right;
-        }
+    /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+     
+    vector<int> inorderTraversal(TreeNode* root) {
+        //base
+       
+       vector < int > inOrder;
+  stack < TreeNode * > s;
+  while (true) {
+    if (root != NULL) {
+      s.push(root);
+      root = root -> left;
+    } else {
+      if (s.empty()) break;
+      root = s.top();
+      inOrder.push_back(root ->val);
+      s.pop();
+      root = root -> right;
     }
-
-    return in;
+  }
+  return inOrder;
+    }
+};
 }
 int main() {
 
